2bfa06f8c0bf14f015d79b8b4f2f2b3c228931fb
Chunpong Chan <122968364+65011212003@users.noreply.github.com>
2024-09-14 00:29:09 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,194 @@
+// import 'package:flutter/material.dart';
+// import 'package:lottorita888/admin_home.dart';
+// import 'package:lottorita888/home.dart';
+// import 'dart:ui';
+// import 'register.dart';
+// import 'package:lottorita888/services/api_service.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'LOTTORITA 69',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: const LoginScreen(),
+//     );
+//   }
+// }
+
+// class LoginScreen extends StatefulWidget {
+//   const LoginScreen({super.key});
+
+//   @override
+//   _LoginScreenState createState() => _LoginScreenState();
+// }
+
+// class _LoginScreenState extends State<LoginScreen> {
+//   final TextEditingController _usernameController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//   bool _isLoading = false;
+
+//   Future<void> _login() async {
+//     setState(() {
+//       _isLoading = true;
+//     });
+
+//     try {
+//       // Check if the username and password are both "admin"
+//       if (_usernameController.text == 'admin' && _passwordController.text == 'admin') {
+//         // Navigate to LotteryAdminPage
+//         Navigator.of(context).pushReplacement(
+//           MaterialPageRoute(builder: (context) => LotteryAdminPage()),
+//         );
+//       } else {
+//         // Proceed with normal login
+//         final result = await ApiService.login(
+//           _usernameController.text,
+//           _passwordController.text,
+//         );
+        
+//         if (result.containsKey('user_id')) {
+//           String userId = result['user_id'].toString();
+//           Navigator.of(context).pushReplacement(
+//             MaterialPageRoute(builder: (context) => HomePage(userId: userId)),
+//           );
+//         } else {
+//           throw Exception('User ID not found in the response');
+//         }
+//       }
+//     } catch (e) {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text('เข้าสู่ระบบล้มเหลว: ${e.toString()}')),
+//       );
+//     } finally {
+//       setState(() {
+//         _isLoading = false;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           Container(
+//             decoration: const BoxDecoration(
+//               image: DecorationImage(
+//                 image: AssetImage("assets/images/background.jpg"),
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//             child: BackdropFilter(
+//               filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+//               child: Container(
+//                 color: Colors.black.withOpacity(0.3),
+//               ),
+//             ),
+//           ),
+//           Center(
+//             child: Padding(
+//               padding: const EdgeInsets.all(20.0),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: <Widget>[
+//                   const Text(
+//                     'LOTTORITA 69',
+//                     style: TextStyle(
+//                       fontSize: 32,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   const Text(
+//                     'ชุดใหญ่ โอนไว จัดเต็ม พร้อมบิด',
+//                     style: TextStyle(
+//                       fontSize: 16,
+//                       color: Color.fromARGB(255, 247, 247, 247),
+//                     ),
+//                   ),
+//                   const SizedBox(height: 30),
+//                   Card(
+//                     child: Padding(
+//                       padding: const EdgeInsets.all(20.0),
+//                       child: Column(
+//                         children: <Widget>[
+//                           TextField(
+//                             controller: _usernameController,
+//                             keyboardType: TextInputType.emailAddress,
+//                             decoration: const InputDecoration(
+//                               labelText: 'อีเมล',
+//                               hintText: 'example@email.com',
+//                             ),
+//                           ),
+//                           const SizedBox(height: 10),
+//                           TextField(
+//                             controller: _passwordController,
+//                             obscureText: true,
+//                             decoration: const InputDecoration(
+//                               labelText: 'รหัสผ่าน',
+//                             ),
+//                           ),
+//                           const SizedBox(height: 20),
+//                           ElevatedButton(
+//                             onPressed: _isLoading ? null : _login,
+//                             style: ElevatedButton.styleFrom(
+//                               foregroundColor: Colors.black,
+//                               backgroundColor: Colors.amber,
+//                             ),
+//                             child: _isLoading
+//                                 ? const CircularProgressIndicator(color: Colors.black)
+//                                 : const Text('เข้าสู่ระบบ'),
+//                           ),
+//                           const SizedBox(height: 10),
+//                           Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                             children: <Widget>[
+//                               TextButton(
+//                                 child: const Text('ลืมรหัสผ่าน'),
+//                                 onPressed: () {
+//                                   // Forgot password logic
+//                                 },
+//                               ),
+//                               TextButton(
+//                                 child: const Text('สมัครสมาชิก'),
+//                                 onPressed: () {
+//                                   Navigator.push(
+//                                     context,
+//                                     MaterialPageRoute(builder: (context) => const RegisterScreen()),
+//                                   );
+//                                 },
+//                               ),
+//                             ],
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:lottorita888/admin_home.dart';
 import 'package:lottorita888/home.dart';
@@ -33,7 +224,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
 
@@ -43,27 +234,26 @@ class _LoginScreenState extends State<LoginScreen> {
     });
 
     try {
-      // Check if the username and password are both "admin"
-      if (_usernameController.text == 'admin' && _passwordController.text == 'admin') {
-        // Navigate to LotteryAdminPage
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => LotteryAdminPage()),
-        );
-      } else {
-        // Proceed with normal login
-        final result = await ApiService.login(
-          _usernameController.text,
-          _passwordController.text,
-        );
-        
-        if (result.containsKey('user_id')) {
-          String userId = result['user_id'].toString();
+      final result = await ApiService.login(
+        _emailController.text,
+        _passwordController.text,
+      );
+      
+      if (result.containsKey('user_id') && result.containsKey('role')) {
+        String userId = result['user_id'].toString();
+        String role = result['role'].toString();
+
+        if (role == 'admin') {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LotteryAdminPage()),
+          );
+        } else {
           Navigator.of(context).pushReplacement(
             MaterialPageRoute(builder: (context) => HomePage(userId: userId)),
           );
-        } else {
-          throw Exception('User ID not found in the response');
         }
+      } else {
+        throw Exception('User ID or role not found in the response');
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -124,7 +314,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Column(
                         children: <Widget>[
                           TextField(
-                            controller: _usernameController,
+                            controller: _emailController,
                             keyboardType: TextInputType.emailAddress,
                             decoration: const InputDecoration(
                               labelText: 'อีเมล',
